2ac08a95e52f93a9bfa93aaedfe52db9fa1da326
ratulchowdhury <ratulchowdhuryofficial@gmail.com>
2026-01-09 11:19:44 +0530

    Adding results

--- simple_agents_demo.py
+++ simple_agents_demo.py
@@ -55,3 +55,16 @@ for step in agent.stream(inputs, stream_mode="values"):
         print(f"Observation: {message.content}")
 
 print("\n--- Finished chain ---")
+
+"""
+
+values] {'messages': [HumanMessage(content='What is the capital of westbengal and what is the weather there?', additional_kwargs={}, response_metadata={}, id='36e0c975-af00-4207-ab69-3d48d5df9c0c'), AIMessage(content='', additional_kwargs={'refusal': None}, response_metadata={'token_usage': {'completion_tokens': 52, 'prompt_tokens': 326, 'total_tokens': 378, 'completion_tokens_details': {'accepted_prediction_tokens': 0, 'audio_tokens': 0, 'reasoning_tokens': 0, 'rejected_prediction_tokens': 0}, 'prompt_tokens_details': {'audio_tokens': 0, 'cached_tokens': 0}}, 'model_provider': 'openai', 'model_name': 'gpt-4o-2024-08-06', 'system_fingerprint': 'fp_deacdd5f6f', 'id': 'chatcmpl-CvzTSpenuFDtWj3vdpxiWq3Qxit1H', 'service_tier': 'default', 'finish_reason': 'tool_calls', 'logprobs': None}, id='lc_run--019ba142-58f0-7a91-9db8-0af90f16ff1e-0', tool_calls=[{'name': 'duckduckgo_search', 'args': {'query': 'capital of West Bengal'}, 'id': 'call_08dVhRbvAnZZsof3GnDfB1qW', 'type': 'tool_call'}, {'name': 'weather_report', 'args': {'location': 'Kolkata'}, 'id': 'call_RzT1jWU8pxIKsNtNJeI4pbio', 'type': 'tool_call'}], invalid_tool_calls=[], usage_metadata={'input_tokens': 326, 'output_tokens': 52, 'total_tokens': 378, 'input_token_details': {'audio': 0, 'cache_read': 0}, 'output_token_details': {'audio': 0, 'reasoning': 0}})]}
+Input Action: duckduckgo_search
+Input Action: weather_report
+Observation: {"request": {"type": "City", "query": "Kolkata, India", "language": "en", "unit": "m"}, "location": {"name": "Kolkata", "country": "India", "region": "West Bengal", "lat": "22.570", "lon": "88.370", "timezone_id": "Asia/Kolkata", "localtime": "2026-01-09 11:04", "localtime_epoch": 1767956640, "utc_offset": "5.50"}, "current": {"observation_time": "05:34 AM", "temperature": 19, "weather_code": 143, "weather_icons": ["https://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0006_mist.png"], "weather_descriptions": ["Haze"], "astro": {"sunrise": "06:18 AM", "sunset": "05:09 PM", "moonrise": "10:59 PM", "moonset": "10:22 AM", "moon_phase": "Waning Gibbous", "moon_illumination": 66}, "air_quality": {"co": "789.85", "no2": "15.35", "o3": "79", "so2": "23.95", "pm2_5": "102.55", "pm10": "104.45", "us-epa-index": "4", "gb-defra-index": "4"}, "wind_speed": 13, "wind_degree": 343, "wind_dir": "NNW", "pressure": 1019, "precip": 0, "humidity": 60, "cloudcover": 0, "feelslike": 19, "uv_index": 6, "visibility": 2, "is_day": "yes"}}
+Final Output: The capital of West Bengal is Kolkata.
+Final Output: The capital of West Bengal is Kolkata.
+
+The current weather in Kolkata is hazy with a temperature of 19°C. The wind is blowing from the NNW at 13 km/h, and the humidity is at 60%. The visibility is reduced to 2 km due to the haze.
+
+"""
